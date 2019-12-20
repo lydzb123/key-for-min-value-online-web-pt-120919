c37796e225cee542ee782f5697bca4d238dd3602
Lydia B <lydiabaik@gmail.com>
2019-12-20 01:20:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.collect do |name, value| end
+  name_hash.collect do |name, value|
+
+  end
+
 end
 
 
-name_hase = {:blake => 500, :ashley => 2, :adam => 1}
+name_hash = {:blake => 500, :ashley => 2, :adam => 1}
